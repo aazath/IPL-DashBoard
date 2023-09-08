c44bc5b5088f7590263640d5f5da1bb096091026
Aazath Abdul Salam <asaazath@gmail.com>
2023-09-08 18:58:23 +0530

    Front-end React App with Team Page

--- ipl-dashboard/src/main/java/lk/ccs/ipldashboard/controller/TeamController.java
+++ ipl-dashboard/src/main/java/lk/ccs/ipldashboard/controller/TeamController.java
@@ -6,6 +6,7 @@ import lk.ccs.ipldashboard.repository.IMatchRepository;
 import lk.ccs.ipldashboard.repository.ITeamRepository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private ITeamRepository teamRepository;
